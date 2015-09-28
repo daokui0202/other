effb0af19166999fdc940cbad3dc317617ffdee2
daokui0202 <daokui0202@163.com>
2015-09-28 14:38:37 +0800

    the one

---
+++ the one/the one/Controller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  the one
+//
+//  Created by wawa on 15/9/28.
+//  Copyright © 2015年 wawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
